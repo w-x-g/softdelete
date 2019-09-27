fcec8b921099865047883b7c049a9e5a9d3e5dfd
ctrlY <772848039@qq.com>
2019-09-27 22:07:12 +0800

    commit

---
+++ src/Builder.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace ultraman\softdelete;
+
+use think\db\connector\Mysql AS TPMysql;
+
+//软删除数据库驱动
+class Mysql Extends TPMysql
+{
+    protected $builder = 'Mysql';
+}
---
+++ src/Query.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace ultraman\softdelete;
+
+use think\db\Query AS TPQuery;
+
+//软删除查询对象
+class Query Extends TPQuery
+{
+    
+}
